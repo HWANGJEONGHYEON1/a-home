39103f5d64da1cc0a3f1781e2fea24e70d7be8dd
jhhwnag <jhhwang@wemakeprice.com>
2023-07-15 11:00:30 +0900

    feat 프로젝트 세팅

---
+++ src/main/java/com/ably/demo/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.ably.demo.config;
+
+import jakarta.servlet.DispatcherType;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().cors().disable()
+                .authorizeHttpRequests(request -> request
+                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
